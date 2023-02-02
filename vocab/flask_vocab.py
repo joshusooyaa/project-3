c73aa4d3329f12c63cf50072d26f36873a7d78c4
Josh Sawyer <thejoshsawyer@gmail.com>
2023-02-02 00:59:40 -0800

    Made working changes

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -5,6 +5,7 @@ from a scrambled string)
 """
 
 import flask
+from flask import request
 import logging
 
 # Our modules
@@ -79,7 +80,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check") # Not sure if I'm supposed to keep the methods=["POST"] - not sure how to fit this in the .getJSON request
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -92,35 +93,37 @@ def check():
     app.logger.debug("Entering check")
 
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    text = request.args.get("text", type=str)
     jumble = flask.session["jumble"]
-    matches = flask.session.get("matches", [])  # Default to empty list
-
+    matches = flask.session.get("matches", []) # Default to empty list
+    
     # Is it good?
     in_jumble = LetterBag(jumble).contains(text)
     matched = WORDS.has(text)
-
+    rslt = None
     # Respond appropriately
     if matched and in_jumble and not (text in matches):
         # Cool, they found a new word
         matches.append(text)
         flask.session["matches"] = matches
+        rslt = {"response": 'match', "txt": ""}
     elif text in matches:
-        flask.flash("You already found {}".format(text))
+        rslt = {"response": "You already found {}".format(text), "txt": ""}
     elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
+        # rslt = {"response": "{} isn't in the list of words".format(text)}
+        result = {"response": "none", "txt": ""}
     elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
+        rslt = {"txt": '"{}" can\'t be made from the letters {}'.format(text, jumble), "response": "incorrect"}
     else:
         app.logger.debug("This case shouldn't happen!")
-        assert False  # Raises AssertionError
-
-    # Choose page:  Solved enough, or keep going?
+        assert False # Raises AssertionError
+    
+    # Choose page: Solved enough, or keep going?
     if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
+        rslt = {"response": "done", "txt": ""}
+        return flask.jsonify(result=rslt)
     else:
-       return flask.redirect(flask.url_for("keep_going"))
+        return flask.jsonify(result=rslt)
 
 
 ###############
